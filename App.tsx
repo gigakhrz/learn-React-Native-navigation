7bffc283c961be7eff046e8be66da671133c002f
gigakhrz <kharazishvilig13@gmail.com>
2023-12-27 10:50:51 +0400

    create project to start practise

---
+++ App.tsx
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+function App(): React.JSX.Element {
+  return <></>;
+}
+
+const styles = StyleSheet.create({});
+
+export default App;

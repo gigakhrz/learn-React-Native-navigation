0ef686dd03efff737f2705545015e4a32fd88b77
gigakhrz <kharazishvilig13@gmail.com>
2023-12-27 10:52:05 +0400

    create Home screen

---
+++ src/screens/Home.tsx
@@ -0,0 +1,13 @@
+import {StyleSheet, Text, View} from 'react-native';
+
+const Home = (): JSX.Element => {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default Home;
